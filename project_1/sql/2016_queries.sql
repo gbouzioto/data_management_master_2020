cc25a7edd6a459cb8639d825dff719a4336e0ced
xavales <greg.bouzios@gmail.com>
2020-12-18 03:19:20 +0200

    Changes v18

--- project_1/sql/2016_queries.sql
+++ project_1/sql/2016_queries.sql
@@ -31,4 +31,36 @@ select b.isbn, b.title
   
 begin transaction;
 
+with q1 as (delete from "2016_book_order" 
+	where order_id in (
+					    select order_id
+					    from "2016_order"
+					    where user_id in
+					          (select distinct u.user_id
+					           from "2016_user" as u,
+					                "2016_order" as o,
+					                "2016_book_order" as bo,
+					                "2016_user_address" as ua,
+					                "2016_address" as a,
+					                "2016_book" as b
+					           where u.user_id = ua.user_id
+					             and a.address_id = ua.address_id
+					             and u.user_id = o.user_id
+					             and o.order_id = bo.order_id
+					             and b.book_id = bo.book_id
+					           limit 1
+      						)
+					    order by order_id DESC
+					    limit 1
+					) returning order_id),
+	q2 as (delete from "2016_order"
+			where order_id in (select q1.order_id from q1) returning user_id, billing_address_id, shipping_address_id),
+	q3 as (insert into "2016_order" (user_id, billing_address_id, shipping_address_id, placement)
+			select q2.user_id, q2.billing_address_id, q2.shipping_address_id, now() from q2 returning order_id)
+	insert into "2016_book_order" (book_id, order_id) select (
+																select distinct b.book_id
+																from "2016_book" as b, "2016_book_order" as bo, "2016_order" as o 
+																where b.book_id = bo.book_id and bo.order_id = o.order_id limit 1  
+															), q3.order_id from q3;
+
 commit;
--- project_1/sql/2016_schema.sql
+++ project_1/sql/2016_schema.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.2 (Ubuntu 12.2-4)
--- Dumped by pg_dump version 12.2 (Ubuntu 12.2-4)
+-- Dumped from database version 12.5 (Ubuntu 12.5-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.5 (Ubuntu 12.5-0ubuntu0.20.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
